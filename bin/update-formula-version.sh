a4c84c0bb6ce2fcc91726d5f675e520d375e0c69
Justen Stall <justen.stall@udri.udayton.edu>
2024-02-28 12:09:41 -0500

    fix: add update-formula action for incorporating updated registry URLs
    Signed-off-by: Justen Stall <justen.stall@udri.udayton.edu>

--- bin/update-formula-version.sh
+++ bin/update-formula-version.sh
@@ -4,7 +4,7 @@ set -e
 # update-formula-version.sh "Formula/act3-pt.rb" "1.0.0"
 
 FORMULA_FILE=$1 # Path to formula to update
-VERSION=$2 # Version to update formula to
+VERSION=$2      # Version to update formula to
 
 # Find something like "version 'v*.*.*'" and replace with the new version number
 sed -i -E "s/version ['\"](.*)['\"]/version \"${VERSION#v}\"/g" "${FORMULA_FILE}"
